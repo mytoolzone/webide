49a9cc4de77a6d8d86860c9c6b450258b13f55e9
gw123 <gw123@qq.com>
2023-05-11 21:44:52 +0800

    添加vscode go python 扩展

--- webide-docker/build.sh
+++ webide-docker/build.sh
@@ -1,5 +1,5 @@
 #CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags  '-w -s  -extldflags "-static"' -o ./server .
-tag=0.0.25
+tag=0.0.27
 APP=webide
 docker build -t xytschool/${APP}:${tag} . -f Dockerfile
 docker push xytschool/${APP}:${tag}
